9d99ecc68f043c53ea3b07f013bc3ea0cfc01c26
JoelTeoGom <joelteodorogomez@gmail.com>
2024-09-25 18:58:00 +0200

    puzzle solved

--- main.go
+++ main.go
@@ -14,8 +14,10 @@ type Puzzle struct {
 
 // Function to check if one row or column is a mirror of another
 func isMirror(row1, row2 []string) bool {
-	for i, j := 0, len(row2)-1; i < len(row1); i, j = i+1, j-1 {
-		if row1[i] != row2[j] {
+	fmt.Println("Row 1: ", row1)
+	fmt.Println("row 2: ", row2)
+	for i := 0; i < len(row1); i++ {
+		if row1[i] != row2[i] {
 			return false
 		}
 	}
@@ -23,8 +25,11 @@ func isMirror(row1, row2 []string) bool {
 }
 
 // Function to find mirrors in rows (horizontal)
-func horizontal(puzzle *Puzzle, sumarize *int) {
+func horizontal(puzzle *Puzzle, sumarize *int, nPuzzle int) {
+	fmt.Println("Horizontal [", nPuzzle, "]")
+
 	for i := 0; i < puzzle.Row-1; i++ { // -1 to avoid index out of range
+		//fmt.Println(puzzle.data[i])
 		if isMirror(puzzle.data[i], puzzle.data[i+1]) {
 			puzzle.Reflection = i + 1
 			fmt.Printf("Found a mirror in rows %d and %d\n", i, i+1)
@@ -36,8 +41,9 @@ func horizontal(puzzle *Puzzle, sumarize *int) {
 }
 
 // Function to find mirrors in columns (vertical)
-func vertical(puzzle *Puzzle, sumarize *int) {
+func vertical(puzzle *Puzzle, sumarize *int, nPuzzle int) {
 	column := make([][]string, puzzle.Col)
+	fmt.Println("Vertical[", nPuzzle, "]")
 
 	// Transpose the puzzle data into columns
 	for i := 0; i < puzzle.Row; i++ {
@@ -49,6 +55,7 @@ func vertical(puzzle *Puzzle, sumarize *int) {
 	puzzle.Reflection = 0
 	// Compare columns to find mirrors
 	for i := 0; i < puzzle.Col-1; i++ { // -1 to avoid index out of range
+		//fmt.Println(column[i])
 		if isMirror(column[i], column[i+1]) {
 			puzzle.Reflection = i + 1
 			fmt.Printf("Found a mirror in columns %d and %d\n", i, i+1)
@@ -60,6 +67,8 @@ func vertical(puzzle *Puzzle, sumarize *int) {
 }
 
 func readfile(sumarize *int, puzzle *Puzzle, filename string) {
+	var nPuzzle = 0
+
 	file, err := os.Open(filename)
 	if err != nil {
 		fmt.Println("Error opening file:", err)
@@ -69,17 +78,19 @@ func readfile(sumarize *int, puzzle *Puzzle, filename string) {
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
+
 		line := scanner.Text()
 		if line == "" { // Check for empty lines
 			//fmt.Println("Puzzle data before reset:", puzzle.data)
-			vertical(puzzle, sumarize)
-			horizontal(puzzle, sumarize)
+			vertical(puzzle, sumarize, nPuzzle)
+			horizontal(puzzle, sumarize, nPuzzle)
 
 			// Reset the puzzle for the next segment
 			puzzle.data = nil
 			puzzle.Row = 0
 			puzzle.Col = 0
 			puzzle.Reflection = 0
+			nPuzzle++
 			continue // Skip to the next iteration
 		}
 
@@ -98,8 +109,10 @@ func readfile(sumarize *int, puzzle *Puzzle, filename string) {
 	}
 
 	//for the last
-	vertical(puzzle, sumarize)
-	horizontal(puzzle, sumarize)
+	vertical(puzzle, sumarize, nPuzzle)
+	horizontal(puzzle, sumarize, nPuzzle)
+	nPuzzle++
+	fmt.Println("npuzzle: ", nPuzzle)
 
 }
 
